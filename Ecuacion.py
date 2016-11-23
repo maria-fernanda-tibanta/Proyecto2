4285d868360f08238b992ae7b82023c2ccf2bcd2
USUARIO03-PC\Usuario01 <fertibanta@hotmail.com>
2016-11-23 12:53:57 -0500

    Resultado en archivo.txt

--- Ecuacion.py
+++ Ecuacion.py
@@ -7,15 +7,36 @@ def ecuacion():
     c=int(input("Ingrese el valor de c: "))
     if (a<=0 ):
         print("el valor de a debe ser mayor a cero")
-    elif (a>0):
-        resul1=b**2-(4*a*c)
-        raiz=resul1**0.5
-        resul2=(-b+raiz)/2*a
-        resul3=(-b-raiz)/2*a
-        print ("El resultado es: ", resul2 ,"\n", resul3)        
+    else:
+        dis=b**2-(4*a*c)
+        if (dis<=0):
+            print ("No existen raices negativas")
+        if (dis>0):   
+            raiz=dis**0.5
+            resul2=(-b+raiz)/2*a
+            resul3=(-b-raiz)/2*a
+            #print ("El resultado es: ", resul2 ,"\t", resul3)        
+            grabartxt(resul2,resul3)
+
+def creartxt():
+    archi=open('Resultado.txt','w')
+    archi.close()
+
+def grabartxt(resul2,resul3):
+    print (resul2,resul3)
+    a=str (resul2)
+    b=str (resul3)
+    archi=open('Resultado.txt','a')
+    archi.write ("El resultado es: " + a + b)
+    archi.close()
 
 
+creartxt()
 ecuacion()
+
+
+
+
 
 
 
